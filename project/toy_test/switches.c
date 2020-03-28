e3cbfae362b179c83dc3da17bfb9506f85330e8a
Fabian Cano <facano2@miners.utep.edu>
2020-03-27 23:09:23 -0600

    added switch version of button states

--- project/toy_test/switches.c
+++ project/toy_test/switches.c
@@ -6,7 +6,7 @@
 #include "stateMachine.h"
 void turnOff();
 void button_state(); // added these two for the methods in assembly
-
+int state;
 char switch_state_down, switch_state_changed, s1, s2, s3, s4; /* effectively boolean */
 int beat = 600;// used to cahnge the beat of the tune
 
@@ -60,26 +60,43 @@ switch_interrupt_handler()
 {
 
   char p1val = switch_update_interrupt_sense();
-  // sets up values for the switches to check if pressed then te if statements check and carry out an action depending the button pressed more in README.md
+  // sets up values for the switches to check if pressed then te if statements check and carry out      an action depending the button pressed more in README.md
   s1 = (p1val & SW1) ? 0 : 1;
   s2 = (p1val & SW2) ? 0 : 1;
   s3 = (p1val & SW3) ? 0 : 1;
   s4 = (p1val & SW4) ? 0 : 1;
-   button_state();
-   /* I made the fallowing into switch cases then to a function in assembly called button_state();
-  if (s1){
+  button_state();
+   /*
+  if(s1) {
+    state = 1;
+  }
+  else if(s2) {
+
+    state = 2;
+  }
+  else if(s3) {
+    state = 3;
+  }
+  else if(s4) {
+    state = 4;
+
+  }
+   */
+   
+   /* I made the fallowing into switch cases (below) then to a function in assembly called button_s      tate();
+  if (s1) {
     switch_state_down = s1;
     switch_state_changed = 1;
     buzzer_set_period(1000);
     dim1 = 1;
   }
 
-  else if(s2){
+  else if(s2) {
       switch_state_down = s2;
       switch_state_changed = 1;
       buzzer_set_period(beat);
       beat = beat + 50;// used to change the beat every time its pressed.
-      if (beat >2000){
+      if (beat >2000) {
 	beat = 600;
       }
       led_init();
@@ -89,18 +106,62 @@ switch_interrupt_handler()
       green_on =1;
       led_update();
   }
-  else if(s3){
+  else if(s3) {
     switch_state_down = s3;
     switch_state_changed = 1;
     turnOff();
     dim1 =0;
   }
 
-  else if(s4){
+  else if(s4) { 
     switch_state_down = s4;
     switch_state_changed = 1;
     playSong();
     dim1 =0;
   }
+   */
+   /*
+     switch(state) {   // This is the switch version of the previos if statements for the state of 
+                         bottons wich i then changed to assembly
+ case 1:
+     switch_state_down = s1;
+     switch_state_changed = 1;
+     buzzer_set_period(1000);
+     dim1 = 1;
+     state =0;
+     break;
+ case 2:
+     switch_state_down = s2;
+     switch_state_changed = 1;
+     buzzer_set_period(beat);
+     beat = beat + 50;// used to change the beat every time its pressed.
+     if (beat >2000){
+       beat = 600;
+     }
+     led_init();
+     toggle_red();
+     dim1 = 0;
+     green_on =1;
+     led_update();
+     state =0;
+     break;
+
+   case 3:
+     switch_state_down = s3;
+     switch_state_changed = 1;
+     turnOff();
+     dim1 =0;
+     state= 0;
+     break;
+   case 4:
+     switch_state_down = s4;
+     switch_state_changed = 1;
+     playSong();
+     dim1 =0;
+     state=0;
+     break;
+   default:
+     break;
+   }
    */
 }
